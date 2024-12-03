c4cb5bbb2984852511341adeb3fe47d3eac6518b
Daniel Thompson <dthompson1@unm.edu>
2024-12-03 15:42:21 -0700

    working on prim

--- src/Prim.java
+++ src/Prim.java
@@ -1,13 +1,16 @@
 public class Prim {
     public void createMST(DLinkedList adjecency, int size){
         boolean[] inMST=new boolean[size];
+        boolean[] discovered = new  boolean[size];
         int [] distance=new int[size];
         int [] parent=new int[size];
+        int [] path = new int[size];
         for (int i=0;i<size;i++) {
             distance[i] = Integer.MAX_VALUE;
             inMST[i] = false;
         }
         distance[0]=0;
+
 
     }
 }
