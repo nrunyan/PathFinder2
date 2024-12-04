0a0b1a664409c2a34835758efe26007f3db620e4
Daniel Thompson <dthompson1@unm.edu>
2024-12-04 12:33:06 -0700

    working on prim

--- src/Prim.java
+++ src/Prim.java
@@ -1,11 +1,10 @@
 public class Prim {
-    public void createMST(DLinkedList[][] adjecency, int size,
+    public int[] createMST(DLinkedList[][] adjecency, int size,
                           int numCol,
                           int numRow){
         boolean[] inMST=new boolean[size];
         int [] key=new int[size];
         int [] parent=new int[size];
-        int [] path = new int[size];
         for (int i=0;i<size;i++) {
             key[i] = Integer.MAX_VALUE;
             inMST[i] = false;
@@ -20,11 +19,14 @@ public class Prim {
             int row = nextMin/numCol;
             DLinkedList adjecentTOInMST = adjecency[row][col];
             for(int v = 0; v < adjecentTOInMST.size;v++){
-
+                int currV = (int) adjecentTOInMST.get(v);
+                if(inMST[currV] && key[currV] == Integer.MAX_VALUE){
+                    parent[currV] = nextMin;
+                    key[currV] = 1;
+                }
             }
         }
-
-
+        return parent;
     }
     private int extractMin(int[] key,boolean[] inMST){
         int min = Integer.MAX_VALUE;
@@ -37,5 +39,11 @@ public class Prim {
             }
         }
         return minIndex;
+    }
+    public static void main(String[] args) {
+        char[][] board = ReadConfig.parseFile("testCases/baseTest.txt");
+        ParseBoard parseBoard = new ParseBoard(board,35,7,5);
+        Prim prim = new Prim();
+//        int[] mst = prim.createMST(parseBoard.adjecency,35,7,5);
     }
 }
